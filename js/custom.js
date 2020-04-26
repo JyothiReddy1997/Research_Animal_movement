351798f41942bb8c6d6275a978fc588eeddf5d1f
JyothiReddy1997 <bhavanam.jyothi@gmail.com>
2020-04-26 12:02:46 -0700

    commit

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+
+  $(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+  });
+
+
+
+    function initParallax() {
+    $('#home').parallax("50%", 0.3);
+
+  }
+  initParallax();
+
+  $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });   
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+      })
+
